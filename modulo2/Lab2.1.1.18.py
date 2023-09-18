80fa7e16f820197eb1a7faf4f93d1f3e39587e12
vicvapurub <142956050+vicvapurub@users.noreply.github.com>
2023-09-18 11:25:21 -0600

    Add files via upload
    Lab_2.1.1.6 Lab_2.1.1.18 Lab_2.1.1.19

---
+++ modulo2/Lab2.1.1.18.py
@@ -0,0 +1,8 @@
+﻿'''
+    Nombre: VICTOR MANUEL RAMIREZ REYES
+    Fecha: 18/09/2023
+    Laboratorio: 2.1.1.18
+'''
+
+print("Fundamentos","Programación","en", sep="***", end="...")
+print("Python")
---
+++ modulo2/Lab2.1.1.19.py
@@ -0,0 +1,14 @@
+﻿'''
+    Nombre: VICTOR MANUEL RAMIREZ REYES
+    Fecha: 18/09/2023
+    Laboratorio: 2.1.1.19
+'''
+print("    *"*2)
+print("   * *"*2)
+print("  *   *"*2)
+print(" *     *"*2)
+print("***   ***"*2)
+print("  *   *"*2)
+print("  *   *"*2)
+print("  *****"*2)
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
