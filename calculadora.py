21571208ea90694452ff14921b77b2347f30155e
[vicvapurub] <[manerys1@gmail.com]>
2023-09-25 09:33:35 -0600

    comentario

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+'''
+Autor: Victor Ramirez
+Fecha: 11 septiembre
+Descripción: Iniciando con python
+'''
+a = input("Ingresa el primer valor: ")
+b = input("Ingresa el segundo valor: " )
+print (type (a), type(b))
+print ("Suma" , a + b)
+a = int(a)
+b = int(b)
+print (type (a), type(b))
+print ("Suma: " , a + b)
+print ("Resta: " , a - b)
+print ("Multiplicación: " , a * b)
+print ("Division: " , a / b)
