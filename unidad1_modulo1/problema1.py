b02cf86931ae43f7ac604bdb87a1ba1d2a601520
[vicvapurub] <[manerys1@gmail.com]>
2023-09-25 11:28:55 -0600

    Ejercicios basicos unidad 1

---
+++ unidad1_modulo1/problema1.py
@@ -0,0 +1,9 @@
+'''
+Descripción: Escribir un programa que muestre por pantalla el resultado de la siguiente 
+operación aritmética 
+Autor: Víctor Manuel Ramírez Reyes
+Fecha de creación: 25/09/2023
+
+'''
+
+print (((3+2)/2.5)**2) #imprimir el resultado de las operaciones que estan entre los parentesis
---
+++ unidad1_modulo1/problema2.py
@@ -0,0 +1,11 @@
+'''
+Descripción: Escribir un programa que lea un entero positivo, n, introducido por el 
+usuario y después muestre en pantalla la suma de todos los enteros desde 1 
+hasta n.
+
+Autor: Víctor Manuel Ramírez Reyes
+Fecha de creación: 25/09/2023
+
+'''
+n = int(input()) #capturar el numero entero 
+print ((n*(n+1))/2)
