81392706af78caa2e87788ebe4b8a9621c67a310
[vicvapurub] <[manerys1@gmail.com]>
2023-10-02 10:31:26 -0600

    02/10/2023

---
+++ modulo3/3.1.1.11.py
@@ -0,0 +1,15 @@
+﻿'''
+Nombre: Víctor Manuel Ramírez Reyes
+Fecha: 01/10/2023
+Laboratorio: 3.1.1.11
+
+'''
+income = float(input("Introduce el ingreso anual:"))#capturar un numero decimal y lo guarda en una variable
+
+if income <= 85_528:#condicion para evaluar el ingreso
+    tax= income * 0.18 -556.02#exencion fiscal
+else:
+    tax= 14839.02+(income-85528)*0.32 #si se excede
+
+tax = round(tax, 0)#que el resultado no tenga decimales y sea igual a cero
+print("El impuesto es:", tax, "pesos")#imprimir el resultado 
---
+++ modulo3/3.1.1.12.py
@@ -0,0 +1,19 @@
+﻿'''
+Nombre: Víctor Manuel Ramirez Reyes
+Fecha: 02/10/2023
+Laboratorio: 3.1.1.12
+
+'''
+
+year = int(input("Introduce un año:"))#captura un numero entero y lo guarda en una variable
+
+if year < 1582:#verificar primero si esta dentro del calendario Gregoriano
+    print("No dentro del período del calendario Gregoriano")
+elif year%4 != 0:#si no es divisible entre 4
+    print("Año Común")
+elif year%100 != 0:#si no es divisible entre 100
+    print("Año Bisiesto")
+elif year%400 != 0:#si no es divisible entre 400
+    print("Año Común")
+else:
+    print("Año Bisiesto")
